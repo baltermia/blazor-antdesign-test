732aa5ba8d170c7b08d1c7d7d2ab0122bc303998
speyck <54800864+speyck@users.noreply.github.com>
2021-08-03 10:34:38 +0200

    Create SimpleTable.razor & SimpleTble.razor.cs

---
+++ blazor-antdesign-test/Shared/SimpleTable.razor.cs
@@ -0,0 +1,21 @@
+﻿using AntDesign;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace blazor_antdesign_test.Shared
+{
+    public partial class SimpleTable
+    {
+        [Parameter]
+        public string DividerTopMargin { get; set; }
+
+        [Parameter]
+        public string FormSize { get; set; } = AntSizeLDSType.Default;
+
+        [Parameter]
+        public Dictionary<string, string> TableValues { get; set; }
+
+        [Parameter]
+        public int SpanWidth { get; set; } = 8;
+    }
+}
